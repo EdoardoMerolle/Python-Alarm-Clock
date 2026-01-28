2188c295004ce45ea01a1b476b5aff53c2143d40
EdoardoMerolle <c3035334@hallam.shu.ac.uk>
2026-01-28 12:38:24 +0000

    Update main.py

--- SmartDisplay/main.py
+++ SmartDisplay/main.py
@@ -367,8 +367,8 @@ class SmartClockBackend(QObject):
                     self._fetch_weather()
 
         # 2. Update Night Mode
-        #is_night = (now.hour >= 22 or now.hour < 5)
-        is_night = True
+        is_night = (now.hour >= 22 or now.hour < 5)
+        #is_night = True
         if is_night != self._is_night_mode:
             self._is_night_mode = is_night
             self.nightModeChanged.emit()
